79a62fb1f3ae78651d163738c66b20cda27650f2
Andrey Skorodumov <quickandrey@gmail.com>
2022-12-06 22:18:48 -0800

    Added some missing elements to the readme; license badges, and questions tab.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # fuzzy-engine
 
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
 ## Table of Contents
-[Description](#description)\
-[Install](#install)\
-[Usage](#usage)\
-[Contribution Guidelines](#contribution-guidelines)\
-[Testing Instructions](#testing-instructions)
+[Description](#description)[Install](#install)[Usage](#usage)[Contributing](#contributing)[Tests](#tests)
+[Questions](#questions)
 
 ## Description
 A README file generator.
@@ -16,9 +16,11 @@ Install node.js to execute index.js.
 ## Usage
 After executing 'node index.js', follow the prompts to populate a README.md file.
 
-## Contribution Guidelines
+## Contributing
 Project is complete - no contributions necssary.
 
-## Testing Instructions
+## Tests
 Follow the usage instructions to execute the program; afterwards you should see a README file appear.
 
+## Questions
+This README was created by tarrol, and I am reachable at quickandrey@gmail.com for any questions, comments, or concerns regarding this project.
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { default: Choices } = require('inquirer/lib/objects/choices');
 
-const generateREADME = ({ projectName, description, install, usage, contrGuidelines, testing }) =>
+const generateREADME = ({ projectName, description, install, usage, contrGuidelines, testing, license, username, email }) =>
   `# ${projectName}
+
+## License
+${license}
 
 ## Table of Contents
 [Description](#description)\
 [Install](#install)\
 [Usage](#usage)\
-[Contribution Guidelines](#contribution-guidelines)\
-[Testing Instructions](#testing-instructions)
+[Contributing](#contributing)\
+[Tests](#tests)
+[Questions](#questions)
 
 ## Description
 ${description}
@@ -20,12 +25,14 @@ ${install}
 ## Usage
 ${usage}
 
-## Contribution Guidelines
+## Contributing
 ${contrGuidelines}
 
-## Testing Instructions
+## Tests
 ${testing}
 
+## Questions
+This README was created by ${username}, and I am reachable at ${email} for any questions, comments, or concerns regarding this project.
 `;
 
 inquirer
@@ -61,10 +68,34 @@ inquirer
         name: 'testing',
         message: 'What are your instructions for testing this project?',
       },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'What license would you like to select?',
+        choices: [`MIT`, `Apache 2.0`, `GNU GPL v3`]
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: 'What is your github username?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What email would you like to set up for questions regarding the project?',
+      },
     ]
   )
   .then((responses) => {
+    if (responses.license == `MIT`) {
+      responses.license = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    } else if (responses.license == `Apache 2.0`) {
+      responses.license = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+    } else if (responses.license == `GNU GPL v3`) {
+      responses.license = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+    }
     const readMEContent = generateREADME(responses);
+
 
     fs.writeFile('README.md', readMEContent, (err) =>
       err ? console.log(err) : console.log('Successfully created README.md')
